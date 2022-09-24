cd9a55bec130c0f1259dc9fdc1f08b23a585e631
Sayyid Hafidzurrahman <sayyid.hafidzurrahman@ui.ac.id>
2022-09-24 13:59:34 +0700

    secondtry

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Sayyid Hafidzurrahman',
+        'npm': '2106651925'
+    }
+    return render(request, "katalog.html", context)
 # TODO: Create your views here.
